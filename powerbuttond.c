c3f8803b8f4d08c1f101a247c7dcd121c9d60e9a
Vicki Pfau <vi@endrift.com>
2026-01-05 20:30:04 -0800

    Track META+F16 instead of LMETA
    Originally I was under the impression the devices were just sending LMETA,
    but they were in fact sending LMETA+F16 and F16 wasn't properly mapped.
    This properly uses META+F16 (with no other modifiers active), tracked per
    input device, as well as mapping the scancodes for some devices where it
    wasn't mapped properly

--- powerbuttond.c
+++ powerbuttond.c
@@ -19,31 +19,53 @@
 
 #define MAX_DEVS 2
 
+#define MOD_LSHIFT 0x01
+#define MOD_RSHIFT 0x02
+#define MOD_SHIFT 0x03
+#define MOD_LCTRL 0x04
+#define MOD_RCTRL 0x08
+#define MOD_CTRL 0x0C
+#define MOD_LALT 0x10
+#define MOD_RALT 0x20
+#define MOD_ALT 0x30
+#define MOD_LMETA 0x40
+#define MOD_RMETA 0x80
+#define MOD_META 0xC0
+
 extern char** environ;
 static bool got_alarm = false;
+
+struct evdev_context {
+	struct libevdev* dev;
+	int modifiers;
+};
 
 void signal_handler(int) {
 	got_alarm = true;
 }
 
-struct libevdev* open_dev(const char* path) {
+bool open_dev(const char* path, struct evdev_context* ctx) {
 	int fd = open(path, O_RDONLY | O_NONBLOCK);
 	if (fd < 0) {
-		return NULL;
+		return false;
 	}
 	struct libevdev* dev;
 	if (libevdev_new_from_fd(fd, &dev) < 0) {
 		close(fd);
-		return NULL;
+		return false;
 	}
-	if (!libevdev_has_event_code(dev, EV_KEY, KEY_POWER)) {
+	bool has_meta = libevdev_has_event_code(dev, EV_KEY, KEY_LEFTMETA) && libevdev_has_event_code(dev, EV_KEY, KEY_F16);
+	bool has_power = libevdev_has_event_code(dev, EV_KEY, KEY_POWER);
+	bool has_lid = libevdev_has_event_code(dev, EV_SW, SW_LID);
+	if (!has_meta && !has_power && !has_lid) {
 		libevdev_free(dev);
-		return NULL;
+		return false;
 	}
-	return dev;
+	ctx->dev = dev;
+	return true;
 }
 
-size_t find_devs(struct libevdev* devs[]) {
+size_t find_devs(struct evdev_context* devs) {
 	struct udev* udev = udev_new();
 	size_t num_devs = 0;
 	if (!udev) {
@@ -80,10 +102,8 @@ size_t find_devs(struct libevdev* devs[]) {
 		}
 		const char* devpath = udev_device_get_devnode(dev);
 		if (devpath) {
-			struct libevdev* evdev = open_dev(devpath);
-			if (evdev) {
+			if (open_dev(devpath, &devs[num_devs])) {
 				printf("Found power button device at %s\n", devpath);
-				devs[num_devs] = evdev;
 				++num_devs;
 			}
 		}
@@ -133,15 +153,14 @@ int main(int argc, char* argv[]) {
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGALRM, &sa, NULL);
 
-	struct libevdev* devs[MAX_DEVS] = {0};
+	struct evdev_context devs[MAX_DEVS] = {0};
 	struct pollfd pfds[MAX_DEVS] = {0};
 	size_t num_devs = 0;
 
 	if (argc >= 2) {
 		int i;
-		for (i = 0; i < argc - 1; ++i) {
-			devs[num_devs] = open_dev(argv[i + 1]);
-			if (devs[num_devs]) {
+		for (i = 0; i < argc - 1 && num_devs < MAX_DEVS; ++i) {
+			if (open_dev(argv[i + 1], &devs[num_devs])) {
 				++num_devs;
 			}
 		}
@@ -153,14 +172,14 @@ int main(int argc, char* argv[]) {
 	}
 	size_t i;
 	for (i = 0; i < num_devs; ++i) {
-		pfds[i].fd = libevdev_get_fd(devs[i]);
+		pfds[i].fd = libevdev_get_fd(devs[i].dev);
 		pfds[i].events = POLLIN;
 	}
 
 	bool press_active = false;
 	while (true) {
 		for (i = 0; i < num_devs; ++i) {
-			pfds[i].fd = libevdev_get_fd(devs[i]);
+			pfds[i].fd = libevdev_get_fd(devs[i].dev);
 			pfds[i].revents = 0;
 		}
 
@@ -173,20 +192,22 @@ int main(int argc, char* argv[]) {
 		}
 
 		for (i = 0; i < num_devs; ++i) {
+			struct evdev_context* ctx = &devs[i];
 			if (!(pfds[i].revents & POLLIN)) {
 				continue;
 			}
 			struct input_event ev;
 			do {
-				res = libevdev_next_event(devs[i], LIBEVDEV_READ_FLAG_NORMAL, &ev);
+				res = libevdev_next_event(ctx->dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
 				while (res == LIBEVDEV_READ_STATUS_SYNC) {
-					res = libevdev_next_event(devs[i], LIBEVDEV_READ_FLAG_SYNC, &ev);
+					res = libevdev_next_event(ctx->dev, LIBEVDEV_READ_FLAG_SYNC, &ev);
 				}
 				if (res != LIBEVDEV_READ_STATUS_SUCCESS) {
 					break;
 				}
 				if (ev.type == EV_KEY) {
-					if (ev.code == KEY_POWER) {
+					switch (ev.code) {
+					case KEY_POWER:
 						if (ev.value == 1) {
 							press_active = true;
 							alarm(1);
@@ -194,9 +215,61 @@ int main(int argc, char* argv[]) {
 							press_active = false;
 							do_press("short");
 						}
-					} else if (ev.code == KEY_LEFTMETA && ev.value == 1) {
-						press_active = false;
-						do_press("long");
+						break;
+					case KEY_LEFTSHIFT:
+						devs[i].modifiers &= ~MOD_LSHIFT;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_LSHIFT;
+						}
+						break;
+					case KEY_RIGHTSHIFT:
+						devs[i].modifiers &= ~MOD_RSHIFT;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_RSHIFT;
+						}
+						break;
+					case KEY_LEFTCTRL:
+						devs[i].modifiers &= ~MOD_LCTRL;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_LCTRL;
+						}
+						break;
+					case KEY_RIGHTCTRL:
+						devs[i].modifiers &= ~MOD_RCTRL;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_RCTRL;
+						}
+						break;
+					case KEY_LEFTALT:
+						devs[i].modifiers &= ~MOD_LALT;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_LALT;
+						}
+						break;
+					case KEY_RIGHTALT:
+						devs[i].modifiers &= ~MOD_RALT;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_RALT;
+						}
+						break;
+					case KEY_LEFTMETA:
+						devs[i].modifiers &= ~MOD_LMETA;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_LMETA;
+						}
+						break;
+					case KEY_RIGHTMETA:
+						devs[i].modifiers &= ~MOD_RMETA;
+						if (ev.value > 0) {
+							devs[i].modifiers |= MOD_RMETA;
+						}
+						break;
+					case KEY_F16:
+						if (ev.value == 1 && (ctx->modifiers & MOD_META) && !(ctx->modifiers & ~MOD_META)) {
+							press_active = false;
+							do_press("long");
+						}
+						break;
 					}
 				} else if (ev.type == EV_SW) {
 					if (ev.code == SW_LID && ev.value == 1) {
@@ -204,7 +277,7 @@ int main(int argc, char* argv[]) {
 						do_press("short");
 					}
 				}
-			} while (libevdev_has_event_pending(devs[i]) > 0);
+			} while (libevdev_has_event_pending(ctx->dev) > 0);
 			if (res == -EINTR && press_active && got_alarm) {
 				press_active = false;
 				do_press("long");
